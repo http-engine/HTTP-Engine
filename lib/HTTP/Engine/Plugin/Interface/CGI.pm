8cd730fe7ac02fba38b33b05154d7888a7c61de8
nyarla <nyarla@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-28 09:24:38 +0000

    I wrote POD of Interface::CGI.

--- lib/HTTP/Engine/Plugin/Interface/CGI.pm
+++ lib/HTTP/Engine/Plugin/Interface/CGI.pm
@@ -2,3 +2,26 @@ package HTTP::Engine::Plugin::Interface::CGI;
 use strict; use warnings; use base qw( HTTP::Engine::Plugin::Interface );
 sub run :Method { $_[1]->handle_request }
 1;
+__END__
+
+=head1 NAME
+
+HTTP::Engine::Plugin::Interface::CGI - CGI interface for HTTP::Engine
+
+=head1 SYNOPSIS
+
+config.yaml:
+
+  plugins:
+    - module: Interface::CGI
+
+=head1 AUTHOR
+
+Naoki Okamura (Nyarla) E<lt>thotep@nyarla.netE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut

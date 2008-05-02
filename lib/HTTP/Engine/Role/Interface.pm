2add3e72249546c2b8637fb9743fa027863a7ec7
daisuke <daisuke@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-02 04:54:27 +0000

    update Interface role docs

--- lib/HTTP/Engine/Role/Interface.pm
+++ lib/HTTP/Engine/Role/Interface.pm
@@ -3,7 +3,7 @@ use strict;
 use Moose::Role;
 with 'MooseX::Object::Pluggable';
 
-requires 'run';
+requires qw(run prepare_request finalize_response);
 
 has handler => (
     is       => 'rw',
@@ -12,3 +12,45 @@ has handler => (
 );
 
 1;
+
+__END__
+
+=head1 NAME
+
+HTTP::Engine::Role::Interface - The Interface Role Definition
+
+=head1 SYNOPSIS
+
+  package HTTP::Engine::Interface::CGI;
+  use Moose;
+  with 'HTTP::Engine::Role::Interface';
+
+=head1 DESCRIPTION
+
+HTTP::Engine::Role::Interface defines the role of an interface in HTTP::Engine.
+
+Specifically, an Interface in HTTP::Engine needs to do at least two things:
+
+=over 4
+
+=item Create a HTTP::Engine::Request object from the client request
+
+If you are on a CGI environment, you need to receive all the data from 
+%ENV and such. If you are running on a mod_perl process, you need to muck
+with $r. 
+
+In any case, you need to construct a valid HTTP::Engine::Request object
+so the application handler can do the real work.
+
+=item Accept a HTTP::Engine::Response object, send it back to the client
+
+The application handler must return an HTTP::Engine::Response object.
+
+In turn, the interface needs to do whatever necessary to present this
+object to the client. In a  CGI environment, you would write to STDOUT.
+In mod_perl, you need to call the appropriate $r->headers methods and/or
+$r->print
+
+=back
+
+=cut

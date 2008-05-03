908f85add3a3da3eb31f08f15d4259af2f204a68
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-03 03:23:03 +0000

    moose way

--- lib/HTTP/Engine/Interface/CGI.pm
+++ lib/HTTP/Engine/Interface/CGI.pm
@@ -2,12 +2,22 @@ package HTTP::Engine::Interface::CGI;
 use Moose;
 with 'HTTP::Engine::Role::Interface';
 
+has request_processor => (
+    is      => 'ro',
+    isa     => 'HTTP::Engine::RequestProcessor',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        HTTP::Engine::RequestProcessor->new(
+            handler                    => $self->handler,
+            should_write_response_line => 0,
+        );
+    },
+    handles => [qw/handle_request/],
+);
+
 sub run {
     my ($self) = @_;
-    my $processor = HTTP::Engine::RequestProcessor->new(
-        handler                    => $self->handler,
-        should_write_response_line => 0,
-    );
     $self->handle_request();
 }
 

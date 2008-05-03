7e455227a314781f49d495d3ad2edde2d705004d
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-03 20:45:57 +0000

    added benchmarking script.

---
+++ tools/benchmark.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+use Benchmark qw/timethese timeit timestr/;
+use HTTP::Engine;
+use IO::Scalar;
+
+my $engine = HTTP::Engine->new(
+    interface => {
+        module => 'CGI',
+        args   => {
+            port            => 9999,
+            request_handler => sub {
+                my $c = shift;
+                $c->res->status(200);
+            },
+        }
+    }
+);
+
+$ENV{REMOTE_ADDR}    = '127.0.0.1';
+$ENV{REQUEST_METHOD} = 'GET';
+$ENV{SERVER_PORT}    = 80;
+
+tie *STDOUT, 'IO::Scalar', \my $out;
+my $t = timeit 10_000 => sub {
+    $engine->run;
+};
+untie *STDOUT;
+
+print timestr($t), "\n";

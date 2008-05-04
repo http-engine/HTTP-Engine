765df4aa48eebee9dd226e95849c8a52d7cfb2f1
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-04 14:38:08 +0000

    small changes.

--- examples/lighty/test_fastcgi.pl
+++ examples/lighty/test_fastcgi.pl
@@ -3,7 +3,6 @@ use strict;
 use warnings;
 warn "OKGE";
 use HTTP::Engine;
-warn "HOGE";
 use Data::Dumper;
 
 HTTP::Engine->new(
@@ -11,12 +10,32 @@ HTTP::Engine->new(
         module => 'FCGI',
         args   => {
             request_handler => sub {
-                warn "HANDLE!";
-                my $c = shift;
-                $c->res->content_type('text/plain');
-                $c->res->body(Dumper($c->req));
+                    my $c = shift;
+
+                    $c->res->content_type('text/html');
+
+                    $c->res->body( render_body( Dumper($c->req) ) );
               }
         },
     },
 )->run;
+
+sub render_body {
+    my @args = @_;
+
+    my $body = <<"...";
+        <form method="post">
+            <input type="text" name="foo" />
+            <input type="submit" />
+        </form>
+
+        <form method="post" enctype="multipart/form-data">
+            <input type="file" name="upload_file" />
+            <input type="submit" />
+        </form>
+...
+
+    $body .= join '', map { "<pre>$_</pre>" } @args;
+    $body;
+}
 

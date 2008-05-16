61ca50a36feeecbd40b96efb86512efc6f055417
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-16 05:26:31 +0000

    r13911@o193-156:  tokuhirom | 2008-05-16 10:53:25 +0900  steal some pods from Cat::Req::Upload. thanks.

--- lib/HTTP/Engine/Request/Upload.pm
+++ lib/HTTP/Engine/Request/Upload.pm
@@ -74,3 +74,49 @@ sub basename {
 __PACKAGE__->meta->make_immutable;
 
 1;
+__END__
+
+=head1 NAME
+
+HTTP::Engine::Request::Upload - handles file upload requests
+
+=head1 METHODS
+
+=over 4
+
+=item basename
+
+Returns basename for "filename".
+
+=item link_to
+
+Creates a hard link to the temporary file. Returns true for success,
+false for failure.
+
+    $upload->link_to('/path/to/target');
+
+=item slurp
+
+Returns a scalar containing the contents of the temporary file.
+
+=item copy_to
+
+Copies the temporary file using File::Copy. Returns true for success,
+false for failure.
+
+    $upload->copy_to('/path/to/targe')
+
+=back
+
+=head1 AUTHORS
+
+Kazuhiro Osawa and HTTP::Engine authors.
+
+=head1 THANKS TO
+
+the authors of L<Catalyst::Request::Upload>.
+
+=head1 SEE ALSO
+
+L<HTTP::Engine>, L<Catalyst::Request::Upload>
+

182755b9a3263d599a484fbf214a0535c84b0894
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-16 05:26:55 +0000

    r13916@o193-156:  tokuhirom | 2008-05-16 11:55:45 +0900  we don't need pod test.

--- tools/coverage_test.sh
+++ tools/coverage_test.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 rm -rf cover_db
 perl Makefile.PL
-HARNESS_PERL_SWITCHES=-MDevel::Cover=+ignore,inc make test
+HARNESS_PERL_SWITCHES=-MDevel::Cover=+ignore,inc,-coverage,statement,branch,condition,path,subroutine make test
 cover
 open cover_db/coverage.html

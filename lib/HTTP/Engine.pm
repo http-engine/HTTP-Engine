04bb8976f6e65994afcd2ca4aa8767f446d8886c
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-17 17:25:45 +0000

    lang/perl/HTTP-Engine: Checking in changes prior to tagging of version 0.0.7.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 12420)
    +++ Changes	(local)
    @@ -1,5 +1,20 @@
     Revision history for Perl extension HTTP::Engine
     
    +0.0.7    2008-05-18T02:23:57+09:00
    +         - good by Class::Component
    +         - nice to meet Moose
    +
    +0.0.6    2008-05-13T16:43:40+09:00
    +         - use URI::QueryParam lestrrat++
    +         - fix body parameters problem
    +
    +0.0.5  2008-05-13T12:14:22+09:00
    +         - oops, s/handle_request/request_handler/ in interface param
    +
    +0.0.4    2008-05-12T18:05:32+09:00
    +         - more moosenize by Class::Component 0.17
    +         - maybe, next release the moose branch
    +
     0.0.3    2008-04-30T02:14:21+09:00
              - add Plugin::Interface::ServerSimple (HTTP::Server::Simple support)    tokuhirom++
              - add Plugin::Interface::CGI    nyarla++

--- lib/HTTP/Engine.pm
+++ lib/HTTP/Engine.pm
@@ -1,7 +1,7 @@
 package HTTP::Engine;
 use Moose;
 use HTTP::Engine::Types::Core qw( Interface );
-our $VERSION = '0.0.6';
+our $VERSION = '0.0.7';
 use HTTP::Engine::Context;
 use HTTP::Engine::Request;
 use HTTP::Engine::Request::Upload;

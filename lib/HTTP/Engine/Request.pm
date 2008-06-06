4736b414b4f7ee7ac0e38fe897a0734ac1501b79
nothingmuch <nothingmuch@d0d07461-0603-4401-acd4-de1884942a52>
2008-06-06 16:31:02 +0000

    Add missing methods from HTTP::Request

--- lib/HTTP/Engine/Request.pm
+++ lib/HTTP/Engine/Request.pm
@@ -258,6 +258,25 @@ sub absolute_url {
     $location;
 }
 
+sub content {
+	my ( $self, @args ) = @_;
+
+	if ( @args ) {
+		croak "The HTTP::Request method 'content' is unsupported when used as a writer, use HTTP::Engine::RequestBuilder";
+	} else {
+		return $self->raw_body;
+	}
+}
+
+sub as_string {
+	my $self = shift;
+	$self->as_http_request->as_string; # FIXME not efficient
+}
+
+sub parse {
+	croak "The HTTP::Request method 'parse' is unsupported, use HTTP::Engine::RequestBuilder";
+}
+
 __PACKAGE__->meta->make_immutable;
 
 1;

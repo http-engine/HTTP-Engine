2a319424041aaef66591a77179c8668034bec5eb
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2008-08-13 07:19:27 +0000

    lang/perl/HTTP-Engine: Checking in changes prior to tagging of version 0.0.13.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 18159)
    +++ Changes	(local)
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTTP::Engine
     
    +         2008-08-13T16:06:19+09:00
    +         - add more test scripts
    +         - test coverage 100%!
    +
     0.0.13_2 2008-08-11T16:47:38+09:00
              - !!! incompatible of version under 0.0.13_1 !!!
                using HTTP::Engine::Compat module if you want compatibility of version under 0.0.13_1

--- lib/HTTP/Engine.pm
+++ lib/HTTP/Engine.pm
@@ -1,7 +1,7 @@
 package HTTP::Engine;
 use Moose;
 use HTTP::Engine::Types::Core qw( Interface );
-our $VERSION = '0.0.13_2';
+our $VERSION = '0.0.13';
 use HTTP::Engine::Request;
 use HTTP::Engine::Request::Upload;
 use HTTP::Engine::Response;

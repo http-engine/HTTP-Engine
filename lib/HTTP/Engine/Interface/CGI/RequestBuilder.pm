b848e6bcea67150bdc68c4025f39c6d5c8f3c90a
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-08-24 06:20:11 +0000

    oops

--- lib/HTTP/Engine/Interface/CGI/RequestBuilder.pm
+++ lib/HTTP/Engine/Interface/CGI/RequestBuilder.pm
@@ -1,6 +1,6 @@
 package HTTP::Engine::Interface::CGI::RequestBuilder;
 use Moose;
-extends 'HTTP::Engine::ResponseWriter::CGI';
+extends 'HTTP::Engine::RequestBuilder::CGI';
 
 no Moose;
 __PACKAGE__->meta->make_immutable;

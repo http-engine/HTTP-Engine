01f824c5c20882933168862131ed19f154c6382e
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-01 03:10:33 +0000

    lang/perl/HTTP-Engine: Checking in changes prior to tagging of version 0.0.19.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 26111)
    +++ Changes	(local)
    @@ -1,5 +1,18 @@
     Revision history for Perl extension HTTP::Engine
     
    +0.0.19   2008-12-01T11:44:14+09:00
    +         # next release is 0.1.x series
    +         # no Moose, use Shika HTTP::Headers::Fast more fast and tiny memory futures
    +         - Interface::ServerSimple
    +           fixed bug no headers request's problem miyagawa++
    +         - lazy loading
    +           HTTP::Request, HTTP::Body, File::Copy, File::Spec::Unix, File::stat, CGI::Simple::Cookie
    +         - test
    +           check with backslash separator instead of slash on win32
    +           added test case for HTTP::Engine::Request::Upload
    +         - remove dependency
    +           IO::Socket, IO::Handle, IO::File
    +
     0.0.18   2008-11-14T18:00:54+09:00
              - Interface::ServerSimple
                base uri build add source from Host header

--- lib/HTTP/Engine.pm
+++ lib/HTTP/Engine.pm
@@ -2,7 +2,7 @@ package HTTP::Engine;
 use 5.00800;
 use Moose;
 use HTTP::Engine::Types::Core qw( Interface );
-our $VERSION = '0.0.18';
+our $VERSION = '0.0.19';
 use HTTP::Engine::Request;
 use HTTP::Engine::Request::Upload;
 use HTTP::Engine::Response;

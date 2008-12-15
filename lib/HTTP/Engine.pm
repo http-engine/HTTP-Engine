44c6434035db51801757db28e74e6885f07afd23
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-15 08:36:49 +0000

    lang/perl/HTTP-Engine: Checking in changes prior to tagging of version 0.0.21.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 27373)
    +++ Changes	(local)
    @@ -1,5 +1,11 @@
     Revision history for Perl extension HTTP::Engine
     
    +0.0.21   2008-12-15T17:28:02+09:00
    +	 # maintenance release
    +         - Interface::ServerSimple: fixed to client does not send a header problem bug
    +         - fixed bug content-length cannot make
    +         - test: fixed Cleaning up /tmp directory (rt #41356) ANDK++ tokuhirom++
    +
     0.0.20   2008-12-11T12:01:40+09:00
     	 # maintenance release
              - [BUG] Interface::ServerSimple?: cleanup HTTP::Headers::Fast instance in each request... orz

--- lib/HTTP/Engine.pm
+++ lib/HTTP/Engine.pm
@@ -2,7 +2,7 @@ package HTTP::Engine;
 use 5.00800;
 use Moose;
 use HTTP::Engine::Types::Core qw( Interface );
-our $VERSION = '0.0.20';
+our $VERSION = '0.0.21';
 use HTTP::Engine::Request;
 use HTTP::Engine::Request::Upload;
 use HTTP::Engine::Response;

abe2792b6a2aa681dae5ed49341971ac34098cff
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-10 09:56:15 +0000

    Checking in changes prior to tagging of version 0.1.2.  Changelog diff is:
    Index: Changes
    ===================================================================
    --- Changes	(revision 29833)
    +++ Changes	(working copy)
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTTP::Engine
     
    +0.1.2    2009-02-10T18:47:51+09:00
    +         - depend to latest version(Mouse 0.16)
    +         - added to Middleware notice documentation
    +
     0.1.1    2009-02-06T11:47:54+09:00
              - new dependence to Mouse 0.15 and MouseX::Types
              - added to HTTP::Engine::Cookbook documentation # gugod++

--- lib/HTTP/Engine.pm
+++ lib/HTTP/Engine.pm
@@ -1,7 +1,7 @@
 package HTTP::Engine;
 use 5.00800;
 use Mouse;
-our $VERSION = '0.1.1';
+our $VERSION = '0.1.2';
 use HTTP::Engine::Request;
 use HTTP::Engine::Request::Upload;
 use HTTP::Engine::Response;

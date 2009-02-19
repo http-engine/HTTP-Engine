c12adb02c3887fc5d4fc4799b5019e77d27c8c7b
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-19 03:23:16 +0000

    Checking in changes prior to tagging of version 0.1.4_02.  Changelog diff is:
    Index: Changes
    ===================================================================
    --- Changes	(revision 30262)
    +++ Changes	(working copy)
    @@ -1,5 +1,8 @@
     Revision history for Perl extension HTTP::Engine
     
    +0.1.4_02 2009-02-19T12:10:00+09:00
    +         - fixed to some test case for moose and mod_perl
    +
     0.1.4_01 2009-02-18T19:24:39+09:00
              - switched to Any::Moose
              - Interface::ModPerl

--- lib/HTTP/Engine.pm
+++ lib/HTTP/Engine.pm
@@ -1,7 +1,7 @@
 package HTTP::Engine;
 use 5.00800;
 use Any::Moose;
-our $VERSION = '0.1.4_01';
+our $VERSION = '0.1.4_02';
 use HTTP::Engine::Request;
 use HTTP::Engine::Request::Upload;
 use HTTP::Engine::Response;

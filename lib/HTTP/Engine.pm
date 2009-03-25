18a03a97ab9ae7d6c66cb2ad69a554aec8e3482a
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2009-03-25 09:04:13 +0000

    Checking in changes prior to tagging of version 0.1.5.  Changelog diff is:
    Index: Changes
    ===================================================================
    --- Changes	(revision 31546)
    +++ Changes	(working copy)
    @@ -1,5 +1,10 @@
     Revision history for Perl extension HTTP::Engine
     
    +0.1.5    2009-03-25T17:44:49+09:00
    +         - Interface::ServerSimple
    +           Net::Server configuration support
    +           wrote of attributes documentation
    +
     0.1.4_04 2009-03-13T13:23:57+09:00
              - add HTTP::Engine::Test::Request
              - Interface::Test

--- lib/HTTP/Engine.pm
+++ lib/HTTP/Engine.pm
@@ -1,7 +1,7 @@
 package HTTP::Engine;
 use 5.00800;
 use Any::Moose;
-our $VERSION = '0.1.4_04';
+our $VERSION = '0.1.5';
 use HTTP::Engine::Request;
 use HTTP::Engine::Response;
 use HTTP::Engine::Types::Core qw( Interface );

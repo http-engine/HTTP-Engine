0d8d52ad722cfe1c4dee8b69bfaf7bcd921fefb9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-06-08 20:27:43 +0900

    latest Mouse supports ->import({into_level => n})

--- lib/HTTP/Engine/Interface.pm
+++ lib/HTTP/Engine/Interface.pm
@@ -36,12 +36,7 @@ sub import {
 
     init_class($caller);
 
-    if (Any::Moose::moose_is_preferred()) {
-        Moose->import({ into_level => 1 });
-    }
-    else {
-        Mouse->export_to_level( 1 );
-    }
+    any_moose()->import({into_level => 1});
 }
 
 # fix up Interface.

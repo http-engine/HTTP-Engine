669335701543ae199403b403815bf1c891edb698
yappo <yappo@shibuya.pl>
2009-08-17 08:46:17 +0900

    Checking in changes prior to tagging of version 0.02003.  Changelog diff is:

--- lib/HTTP/Engine.pm
+++ lib/HTTP/Engine.pm
@@ -1,7 +1,7 @@
 package HTTP::Engine;
 use 5.00800;
 use Any::Moose;
-our $VERSION = '0.02002';
+our $VERSION = '0.02003';
 use HTTP::Engine::Request;
 use HTTP::Engine::Response;
 use HTTP::Engine::Types::Core qw( Interface );

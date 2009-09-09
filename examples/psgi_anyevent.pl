cb5951ec481057676b15339baa10222e3ca36837
yappo <yappo@shibuya.pl>
2009-09-09 15:48:45 +0900

    replace example script

--- examples/psgi_anyevent.pl
+++ examples/psgi_anyevent.pl
@@ -2,40 +2,25 @@ use strict;
 use warnings;
 
 use HTTP::Engine;
-use Plack::Impl::AnyEvent;
+use Plack::Loader;
 
-my $plack1 = Plack::Impl::AnyEvent->new(port => 18081);
 my $he1    = HTTP::Engine->new(
     interface => {
         module => 'PSGI',
-        args => {
-            psgi_setup => sub {
-                my $he_handler = shift;
-                $plack1->psgi_app($he_handler);
-                $plack1->run;
-            },
-        },
         request_handler => sub {
             HTTP::Engine::Response->new( body => 'plack 1' );
         },
     },
-)->run;
-
-my $plack2 = Plack::Impl::AnyEvent->new(port => 18082);
+);
 my $he2    = HTTP::Engine->new(
     interface => {
         module => 'PSGI',
-        args => {
-            psgi_setup => sub {
-                my $he_handler = shift;
-                $plack2->psgi_app($he_handler);
-                $plack2->run;
-            },
-        },
         request_handler => sub {
             HTTP::Engine::Response->new( body => 'plach 2' );
         },
     },
-)->run;
+);
 
+Plack::Loader->load('AnyEvent', port => 18081)->run(sub { $he1->run(@_) });
+Plack::Loader->load('AnyEvent', port => 18082)->run(sub { $he2->run(@_) });
 AnyEvent->condvar->recv;
